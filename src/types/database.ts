f1126acb5e885ea76028c68075f7a0fc443dd118
davoodmehraban89 <57283229+davoodmehraban89@users.noreply.github.com>
2026-09-07 08:24:31 +0330

    Add database type definition required by service layer

---
+++ src/types/database.ts
@@ -0,0 +1,11 @@
+export type Database = {
+  public: {
+    Tables: {
+      [key: string]: {
+        Row: Record<string, any>
+        Insert: Record<string, any>
+        Update: Record<string, any>
+      }
+    }
+  }
+}
